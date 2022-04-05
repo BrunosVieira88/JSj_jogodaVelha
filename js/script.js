b1f243155c831963000a1a9f1d21f96767eeff9e
Bruno Santos Vieira <75911878+BrunosVieira88@users.noreply.github.com>
2022-04-05 16:17:30 -0300

    stylos

--- js/script.js
+++ js/script.js
@@ -100,59 +100,101 @@ function vencedor(){
     //segunda linha 
     if(array[4] == 'X' && array[5] == 'X' && array[6] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_4').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_6').style.backgroundColor = "#03F035";
 
     }else if(array[4] == '0' && array[5] == '0' && array[6] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_4').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_6').style.backgroundColor = "#03F035";
     }
 
 
     //terceira linha 
     if(array[7] == 'X' && array[8] == 'X' && array[9] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
+        document.getElementById('troca_8').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
 
     }else if(array[7] == '0' && array[8] == '0' && array[9] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
+        document.getElementById('troca_8').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
     }
 
 
     //1 coluna 
     if(array[1] == 'X' && array[4] == 'X' && array[7] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_1').style.backgroundColor = "#03F035";
+        document.getElementById('troca_4').style.backgroundColor = "#03F035";
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
 
     }else if(array[1] == '0' && array[4] == '0' && array[7] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_1').style.backgroundColor = "#03F035";
+        document.getElementById('troca_4').style.backgroundColor = "#03F035";
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
     }
     
     //2coluna
     if(array[2] == 'X' && array[5] == 'X' && array[8] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_2').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_8').style.backgroundColor = "#03F035";
 
     }else if(array[2] == '0' && array[5] == '0' && array[8] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_2').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_8').style.backgroundColor = "#03F035";
     }
 
     //3 coluna 
     if(array[3] == 'X' && array[6] == 'X' && array[9] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_3').style.backgroundColor = "#03F035";
+        document.getElementById('troca_6').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
 
     }else if(array[3] == '0' && array[6] == '0' && array[9] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_3').style.backgroundColor = "#03F035";
+        document.getElementById('troca_6').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
     }
 
     //1 cruzado 
     if(array[1] == 'X' && array[5] == 'X' && array[9] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_1').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
 
     }else if(array[1] == '0' && array[5] == '0' && array[9] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_1').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_9').style.backgroundColor = "#03F035";
     }
 
     // segundo cruzado 
     if(array[3] == 'X' && array[5] == 'X' && array[7] == 'X' ){
         Swal.fire('Voce Venceu Jogador X');
+        document.getElementById('troca_3').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
 
     }else if(array[3] == '0' && array[5] == '0' && array[7] == '0' ){
         Swal.fire('Voce Venceu Jogador 0');
+        document.getElementById('troca_3').style.backgroundColor = "#03F035";
+        document.getElementById('troca_5').style.backgroundColor = "#03F035";
+        document.getElementById('troca_7').style.backgroundColor = "#03F035";
     }
 
 
